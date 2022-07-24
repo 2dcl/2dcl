c0d459539c826430e96f43c7d592e722b60707d3
Fran Tufro <info@franciscotufro.com>
2022-07-24 14:14:11 -0300

    Formatting fixes

--- 2dcl/src/kernel/main.rs
+++ 2dcl/src/kernel/main.rs
@@ -1,64 +1,65 @@
 use wry::{
-  application::{
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-  },
-  webview::WebViewBuilder,
-  http::ResponseBuilder
+    application::{
+        event::{Event, WindowEvent},
+        event_loop::{ControlFlow, EventLoop},
+        window::WindowBuilder,
+    },
+    http::ResponseBuilder,
+    webview::WebViewBuilder,
 };
 
 pub fn main() -> wry::Result<()> {
-  let event_loop = EventLoop::new();
-  let window = WindowBuilder::new()
-  // .with_decorations(false)
-  // There are actually three layer of background color when creating webview window.
-  // The first is window background...
-  .with_transparent(true)
-  .with_visible(false)
-  .with_title("2dcl kernel")
-  .build(&event_loop)
-  .unwrap();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        // .with_decorations(false)
+        // There are actually three layer of background color when creating webview window.
+        // The first is window background...
+        .with_transparent(true)
+        .with_visible(false)
+        .with_title("2dcl kernel")
+        .build(&event_loop)
+        .unwrap();
 
-  let webview = WebViewBuilder::new(window).unwrap()
-  // The second is on webview...
-  .with_transparent(true)
-  .with_visible(false)
-  // And the last is in html.
-  .with_custom_protocol("wry".into(), move |request| {
-    let path = request.uri()
+    let webview = WebViewBuilder::new(window)
+        .unwrap()
+        // The second is on webview...
+        .with_transparent(true)
+        .with_visible(false)
+        // And the last is in html.
+        .with_custom_protocol("wry".into(), move |request| {
+            let path = request
+                .uri()
                 .replace("wry://", "")
                 .replace("?ws=ws://127.0.0.1:8080", "");
 
-    match path.as_str() {
-      "index" => {
-        ResponseBuilder::new()
-          .mimetype("text/html")
-          .body(include_str!("index.html").as_bytes().to_vec())
-      },
-      _ => ResponseBuilder::new().status(400).body("".as_bytes().to_vec())
+            match path.as_str() {
+                "index" => ResponseBuilder::new()
+                    .mimetype("text/html")
+                    .body(include_str!("index.html").as_bytes().to_vec()),
+                _ => ResponseBuilder::new()
+                    .status(400)
+                    .body("".as_bytes().to_vec()),
+            }
+        })
+        .with_url("wry://index?ws=ws://127.0.0.1:8080")
+        .unwrap();
 
+    #[cfg(debug_assertions)]
+    let webview = webview.with_devtools(true);
+    let webview = webview.build().unwrap();
 
-    }
-  })
-  .with_url("wry://index?ws=ws://127.0.0.1:8080").unwrap();
+    #[cfg(debug_assertions)]
+    webview.open_devtools();
 
-  #[cfg(debug_assertions)]
-  let webview = webview.with_devtools(true);
-  let webview = webview.build().unwrap();
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
 
-  #[cfg(debug_assertions)]
-  webview.open_devtools();
-
-  event_loop.run(move |event, _, control_flow| {
-    *control_flow = ControlFlow::Wait;
-
-    match event {
-      Event::WindowEvent {
-        event: WindowEvent::CloseRequested,
-        ..
-      } => *control_flow = ControlFlow::Exit,
-      _ => {}
-    }
-  });
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => {}
+        }
+    });
 }
