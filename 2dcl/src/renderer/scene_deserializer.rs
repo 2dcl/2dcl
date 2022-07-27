905da5c58ac947986d6f44614823e3640fbf3eb7
loomstyla <pabloferrari.hh@gmail.com>
2022-07-27 14:24:47 -0300

    PlayerMovement

---
+++ 2dcl/src/renderer/scene_deserializer.rs
@@ -0,0 +1,139 @@
+use bevy::prelude::*;
+use serde::Deserialize;
+use std::fs::File;
+use std::io::BufReader;
+use serde_json::Result;
+
+
+
+#[derive(Deserialize, Debug)]
+struct Scene {
+    name: String,
+    entities: Vec<Entitiy>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Entitiy {
+    name: String,
+    components: Vec<Component>,
+}
+
+
+#[derive(Deserialize, Debug)]
+enum Component{
+    Transform(EntityTransform),
+    SpriteRenderer(SpriteRenderer),
+    CircleCollider(CircleCollider),
+    BoxCollider(BoxCollider),
+    AlphaCollider(AlphaCollider)
+}
+
+#[derive(Deserialize, Debug)]
+struct EntityTransform {
+    location: Vec2,
+    rotation: Vec3,
+    scale: Vec2,
+
+}
+
+#[derive(Deserialize, Debug)]
+struct SpriteRenderer {
+    sprite: String,
+    color: Vec4,
+    layer: i32,
+}
+
+#[derive(Deserialize, Debug)]
+struct CircleCollider {
+    center: Vec2,
+    raius: i32,
+}
+
+#[derive(Deserialize, Debug)]
+struct BoxCollider {
+    center: Vec2,
+    size: Vec2,
+}
+
+#[derive(Deserialize, Debug)]
+struct AlphaCollider {
+    sprite: String,
+    channel: i32,
+}
+
+
+pub struct SceneDeserializerPlugin;
+
+
+impl Plugin for SceneDeserializerPlugin
+{
+    fn build(&self, app: &mut App)
+    {
+         app.add_startup_system(setup);
+    }
+
+}
+
+
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+
+) {
+
+
+    if let Ok(file) = File::open("./assets/scene.json")
+    {
+        let reader = BufReader::new(file);
+        let scene: Result<Scene> = serde_json::from_reader(reader);
+        if scene.is_ok()
+        {
+            let scene = scene.unwrap();
+
+            for entity in  scene.entities.iter()
+            {
+                let mut transform = Transform::identity();
+                let mut texture: Handle<Image> = Handle::default();
+                let mut sprite = Sprite::default();
+                     
+               for component in entity.components.iter()
+               {
+                
+                if let Component::Transform(entity_transform) = component
+                {
+
+                    transform.translation = entity_transform.location.extend(0.0);
+                    transform.rotation = Quat::from_euler(
+                        EulerRot::XYZ,
+                        entity_transform.rotation.x.to_radians(),
+                        entity_transform.rotation.y.to_radians(),
+                        entity_transform.rotation.z.to_radians());
+
+                    transform.scale = entity_transform.scale.extend(1.0);
+                }
+
+                if let Component::SpriteRenderer(sprite_renderer) = component
+                {
+                    sprite.color = Color::Rgba { 
+                        red: sprite_renderer.color.x, 
+                        green: sprite_renderer.color.y, 
+                        blue: sprite_renderer.color.z, 
+                        alpha:  sprite_renderer.color.w};
+                    texture = asset_server.load(&sprite_renderer.sprite);
+                }
+               }
+
+               commands.spawn_bundle(SpriteBundle {
+                transform,
+                sprite: sprite.clone(),
+                texture: texture.clone(),
+                ..default()
+                })
+                .insert(Name::new(entity.name.clone()));
+            }
+        }
+
+    }
+
+}
