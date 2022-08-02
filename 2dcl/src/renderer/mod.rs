608f6417b7576986d15280ce00d9cf8ca116c4ab
loomstyla <pabloferrari.hh@gmail.com>
2022-08-02 19:03:12 -0300

    render_to_texture + animatons deserializer (WIP)

--- 2dcl/src/renderer/mod.rs
+++ 2dcl/src/renderer/mod.rs
@@ -4,11 +4,15 @@ mod player;
 mod debug;
 mod scene_deserializer;
 mod collision;
+mod render_to_texture;
+mod animations;
 
 use player::PlayerPlugin;
+use animations::AnimationsPlugin;
 //use debug::DebugPlugin;
 use collision::CollisionPlugin;
 use scene_deserializer::SceneDeserializerPlugin;
+use render_to_texture::RenderToTexturePlugin;
 
 
 
@@ -16,9 +20,11 @@ pub fn start() {
     App::new()
         .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
+       // .add_plugin(AnimationsPlugin)
         .add_plugin(SceneDeserializerPlugin)
         .add_plugin(PlayerPlugin)
-      //  .add_plugin(DebugPlugin)
+        //.add_plugin(RenderToTexturePlugin)
+       // .add_plugin(DebugPlugin)
         .add_plugin(CollisionPlugin)
         .run();
 }
