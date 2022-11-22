85e48c6f2146f5946f0cca10e05e7b3a2f6f5e8a
loomstyla <pabloferrari.hh@gmail.com>
2022-11-22 13:31:16 -0300

    Update build-windows.bat

--- bin/build-windows.bat
+++ bin/build-windows.bat
@@ -1,6 +1,6 @@
 rd /s /q build
 cargo build --release 
-mkdir build/windows
-copy target/release/2dcl build/windows
-xcopy 2dcl/assets build/windows /e
-cd build/windows
+mkdir build\windows\assets
+xcopy target\release build\windows /e
+xcopy 2dcl\assets build\windows\assets /e
+7z -sdel -ttar a dummy build\windows\* -so | 7z -si -tgzip a build\windows\2dcl-windows-0.1.0.tgz
