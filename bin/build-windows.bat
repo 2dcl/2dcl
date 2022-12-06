48e512b875f230c6fed3e204d287b0ef458721cc
Fran Tufro <hi@frantufro.com>
2022-12-06 16:09:24 -0300

    Fixed folders in build

--- bin/build-windows.bat
+++ bin/build-windows.bat
@@ -3,4 +3,4 @@ cargo build --release
 mkdir build\windows\assets
 copy target\release\2dcl.exe build\windows
 xcopy 2dcl\assets build\windows\assets /e
-7z -tzip -sdel a build\windows\2dcl-windows-0.1.0.zip .\build\windows\*
+7z -tzip -sdel a build\2dcl-windows-0.1.0.zip .\build\windows\*
