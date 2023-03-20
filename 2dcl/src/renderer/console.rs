c0e6d21ecead7f16c6a406c5f8255015450e78fd
loomstyla <pabloferrari.hh@gmail.com>
2023-03-20 11:10:20 -0300

    bevy console

--- 2dcl/src/renderer/console.rs
+++ 2dcl/src/renderer/console.rs
@@ -1,10 +1,11 @@
 use bevy::prelude::*;
 use bevy_console::{reply, AddConsoleCommand, ConsoleCommand, ConsolePlugin};
+use clap::Parser;
 use dcl_common::Parcel;
 
 use crate::renderer::scene_loader::get_parcel_spawn_point;
 
-use super::player::PlayerComponent;
+use crate::components;
 use super::scene_maker::RoadsData;
 use super::scenes_io::SceneFilesMap;
 
@@ -13,14 +14,14 @@ pub struct MyConsolePlugin;
 impl Plugin for MyConsolePlugin {
     fn build(&self, app: &mut App) {
         app.add_plugin(ConsolePlugin)
-            .add_console_command::<TeleportCommand, _, _>(teleport_command)
-            .add_console_command::<WhereCommand, _, _>(where_command);
+            .add_console_command::<TeleportCommand, _>(teleport_command)
+            .add_console_command::<WhereCommand, _>(where_command);
     }
 }
 
 /// Teleports the player to a specific parcel
-#[derive(ConsoleCommand)]
-#[console_command(name = "jump")]
+#[derive(Parser, ConsoleCommand)]
+#[command(name = "jump")]
 struct TeleportCommand {
     parcel_x: String,
     parcel_y: String,
@@ -28,13 +29,13 @@ struct TeleportCommand {
 
 
 /// Prints the current parcel
-#[derive(ConsoleCommand)]
-#[console_command(name = "where")]
+#[derive(Parser, ConsoleCommand)]
+#[command(name = "where")]
 struct WhereCommand;
 
 fn where_command(
   mut where_cmd: ConsoleCommand<WhereCommand>,
-  mut player_query: Query<&PlayerComponent>,
+  mut player_query: Query<&components::Player>,
 )
 {
   let player = player_query.single_mut();
@@ -46,12 +47,12 @@ fn where_command(
 
 fn teleport_command(
     mut tp: ConsoleCommand<TeleportCommand>,
-    mut player_query: Query<(&mut PlayerComponent, &mut Transform)>,
+    mut player_query: Query<(&mut components::Player, &mut Transform)>,
     mut roads_data: ResMut<RoadsData>,
     scene_files_map: Res<SceneFilesMap>,
 ) {
     let (mut player, mut transform) = player_query.single_mut();
-    if let Some(TeleportCommand { parcel_x, parcel_y }) = tp.take() {
+    if let Some(Ok(TeleportCommand { parcel_x, parcel_y })) = tp.take() {
         if let Ok(parcel_x) = parcel_x.parse::<i16>() {
             if let Ok(parcel_y) = parcel_y.parse::<i16>() {
                 player.current_level = 0;
