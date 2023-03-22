ea000e5a9e3831f3649490d381f564f1be0d43e2
loomstyla <pabloferrari.hh@gmail.com>
2023-03-22 19:17:53 -0300

    transparency changes

--- 2dcl/src/renderer/transparency.rs
+++ 2dcl/src/renderer/transparency.rs
@@ -2,7 +2,7 @@ use bevy::{prelude::*, sprite::collide_aabb::collide};
 
 use crate::{bundles::get_translation_by_anchor, components};
 
-use super::config::{PLAYER_VISIBILITY_BOX, TRANSPARENCY_VALUE_FOR_HIDING_ELEMENTS};
+use super::config::{PLAYER_VISIBILITY_BOX, TRANSPARENCY_VALUE_FOR_HIDING_ELEMENTS, PLAYER_VISIBILITY_BOX_OFFSET};
 
 pub fn update_transparency(mut sprites_query: Query<(&mut Sprite, &components::SpriteRenderer)>) {
     for (mut sprite, sprite_renderer) in sprites_query.iter_mut() {
@@ -47,14 +47,7 @@ pub fn check_elements_on_top_of_player(
             &other_transform.translation(),
             &other_sprite.anchor,
         );
-        if other_transform.translation().z > player_transform.translation().z
-            && collide(
-                player_transform.translation(),
-                PLAYER_VISIBILITY_BOX,
-                other_location,
-                other_size,
-            )
-            .is_some()
+        if is_object_covering_player(player_transform.translation(),other_location,other_size)
         {
             other_sprite_renderer.is_on_top_of_player = true;
         } else {
@@ -63,6 +56,80 @@ pub fn check_elements_on_top_of_player(
     }
 }
 
+
+fn is_object_covering_player(player_location: Vec3, object_location: Vec3, object_size: Vec2) ->bool
+{
+  if object_location.z < player_location.z
+    {
+     return false; 
+    }
+
+  //Check top left
+  if collide(
+      Vec3{
+        x: player_location.x + PLAYER_VISIBILITY_BOX_OFFSET.x - PLAYER_VISIBILITY_BOX.x,
+        y: player_location.y + PLAYER_VISIBILITY_BOX_OFFSET.y + PLAYER_VISIBILITY_BOX.y,
+        z: player_location.z
+      },
+      Vec2::ONE,
+      object_location,
+      object_size,
+    )
+    .is_none()
+    {
+      return false;
+    }
+
+  //Check top right
+  if collide(
+    Vec3{
+      x: player_location.x + PLAYER_VISIBILITY_BOX_OFFSET.x + PLAYER_VISIBILITY_BOX.x,
+      y: player_location.y + PLAYER_VISIBILITY_BOX_OFFSET.y + PLAYER_VISIBILITY_BOX.y,
+      z: player_location.z
+    },
+    Vec2::ONE,
+    object_location,
+    object_size,
+  )
+  .is_none()
+  {
+    return false;
+  }
+
+  //Check bottom right
+  if collide(
+    Vec3{
+      x: player_location.x + PLAYER_VISIBILITY_BOX_OFFSET.x + PLAYER_VISIBILITY_BOX.x,
+      y: player_location.y + PLAYER_VISIBILITY_BOX_OFFSET.y - PLAYER_VISIBILITY_BOX.y,
+      z: player_location.z
+    },
+    Vec2::ONE,
+    object_location,
+    object_size,
+  )
+  .is_none()
+  {
+    return false;
+  }
+
+  //Check bottom left
+  if collide(
+    Vec3{
+      x: player_location.x + PLAYER_VISIBILITY_BOX_OFFSET.x - PLAYER_VISIBILITY_BOX.x,
+      y: player_location.y + PLAYER_VISIBILITY_BOX_OFFSET.y - PLAYER_VISIBILITY_BOX.y,
+      z: player_location.z
+    },
+    Vec2::ONE,
+    object_location,
+    object_size,
+  )
+  .is_none()
+  {
+    return false;
+  }
+
+    true
+}
 pub fn check_elements_overlapping_parcels(
     player_query: Query<&components::Player>,
     mut sprites_query: Query<&mut components::SpriteRenderer, Without<components::Player>>,
