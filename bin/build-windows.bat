1d07fd3ec2155041cd98f585083030fd78c5e6a7
loomstyla <pabloferrari.hh@gmail.com>
2023-08-25 16:28:36 -0300

    version changes

--- bin/build-windows.bat
+++ bin/build-windows.bat
@@ -5,5 +5,5 @@ copy target\release\2dcl.exe build\windows
 copy target\release\ethereum-adapter-webserver.exe build\windows
 copy 2dcl\config.toml build\windows
 xcopy 2dcl\assets build\windows\assets /e
-7z -tzip -sdel a build\2dcl-windows-0.1.0.zip .\build\windows\*
+7z -tzip -sdel a build\2dcl-windows-0.2.0.zip .\build\windows\*
 rd build\windows
