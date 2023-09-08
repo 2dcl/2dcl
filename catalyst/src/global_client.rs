8c7fb3b63046cd6b112b8e9e63b22232ef8507e9
loomstyla <pabloferrari.hh@gmail.com>
2023-09-08 14:17:22 -0300

    /stats/parcels

--- catalyst/src/global_client.rs
+++ catalyst/src/global_client.rs
@@ -3,6 +3,24 @@ use dcl_common::Result;
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize, Clone, PartialEq)]
+pub struct ParcelStatsResponse {
+    pub parcels: Vec<ParcelStat>,
+}
+
+#[derive(Debug, Deserialize, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct ParcelStat {
+    pub peers_count: u32,
+    pub parcel: Parcel,
+}
+
+#[derive(Debug, Deserialize, Clone, PartialEq)]
+pub struct Parcel {
+    pub x: i32,
+    pub y: i32,
+}
+
+#[derive(Debug, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "camelCase")]
 pub struct AboutResponse {
     pub healthy: bool,
@@ -68,9 +86,16 @@ pub struct GlobalClient {}
 
 impl GlobalClient {
     /// Returns detailed information about the services health and its configuration
-    /// [See on Catalyst API Docs](https://decentraland.github.io/catalyst-api-specs/#tag/Global)
+    /// [See on Catalyst API Docs](https://decentraland.github.io/catalyst-api-specs/#tag/Global/operation/getAboutCatalystInfo)
     pub async fn about(server: &Server) -> Result<AboutResponse> {
         let result = server.get("/about").await?;
+        Ok(result)
+    }
+
+    /// Returns information about the amount of users on each parcel
+    /// [See on Catalyst API Docs](https://decentraland.github.io/catalyst-api-specs/#tag/Global/operation/getStatsParcels)
+    pub async fn parcels_stats(server: &Server) -> Result<ParcelStatsResponse> {
+        let result = server.get("/stats/parcels").await?;
         Ok(result)
     }
 }
@@ -97,6 +122,26 @@ mod tests {
         m.assert();
 
         let expected: AboutResponse = serde_json::from_str(response).unwrap();
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn it_implements_parcels_stats() {
+        let response = include_str!("../fixtures/parcels_stats.json");
+        let server = MockServer::start();
+
+        let m = server.mock(|when, then| {
+            when.method(GET).path("/stats/parcels");
+            then.status(200).body(response);
+        });
+
+        let server = Server::new(server.url(""));
+        let result: ParcelStatsResponse =
+            tokio_test::block_on(GlobalClient::parcels_stats(&server)).unwrap();
+
+        m.assert();
+
+        let expected: ParcelStatsResponse = serde_json::from_str(response).unwrap();
         assert_eq!(result, expected);
     }
 }
